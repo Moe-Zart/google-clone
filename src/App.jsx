8b81148296d5a0aad6ebc029c62fa8ff4fb13007
Moe-Zart <mohamedzreika03@outlook.com>
2022-07-23 23:19:49 +1000

    header commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
 import "./App.css";
 import HomePage from "./pages/HomePage";
-import Search from "./pages/Search";
-
+import Results from "./pages/Results";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 function App() {
   return (
     <div className="app">
-      {
-        //Home
-      }
-      <HomePage />
-
-      {
-        //Search (the results)
-      }
-      <Search />
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <HomePage />
+          </Route>
+          <Route path="/results">
+            <Results />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,35 @@
-import React from 'react';
-
+import React from "react";
+import { Link } from "react-router-dom";
+import "./HomePage.css";
+import AppsIcon from "@mui/icons-material/Apps";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 const HomePage = () => {
-    return (
-        <div>
-            
+  return (
+    <div className="home">
+      <div className="home__header">
+        <div className="home__headerLeft">
+          <Link to="/about">About</Link>
+          <Link to="/store">Store</Link>
         </div>
-    );
-}
+        <div className="home__headerRight">
+          <Link to="/gmail">Gmail</Link>
+          <Link to="/images">Images</Link>
+          <AppsIcon className="headerIcon" />{" "}
+          <AccountCircleIcon className="headerIcon avatarIcon" />
+        </div>
+      </div>
+      <div className="home__body">
+        <img
+          src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
+          className="home__googleImg"
+          alt=""
+        />
+        <figure className="home__searchBarContainer">
+            <input type="search" />
+        </figure>
+      </div>
+    </div>
+  );
+};
 
 export default HomePage;
